4d44fc584aa9c490eba02adf55fdfbb5d4b844e3
Arne Goeteyn <goeteynarne@gmail.com>
2023-12-13 17:03:10 +0100

    update

--- process.py
+++ process.py
@@ -1,11 +1,9 @@
 import logging
-import random
-import time
-import requests
-import os
 import json
 import urllib.request
-from dv_utils import default_settings, Client, audit_log 
+from dv_utils import default_settings, Client, audit_log
+from dv_utils.connectors import simple_file
+from dv_utils.connectors.connector import populate_configuration
 
 logger = logging.getLogger(__name__)
 
@@ -16,54 +14,52 @@ logging.basicConfig(
 )
 
 # define an event processing function
+
+
 def event_processor(evt: dict):
     """
     Process an incoming event
     Exception raised by this function are handled by the default event listener and reported in the logs.
     """
-    
+
     logger.info(f"Processing event {evt}")
 
     # dispatch events according to their type
-    evt_type =evt.get("type", "")
-    if(evt_type.startswith("TEST_")):
+    evt_type = evt.get("type", "")
+
+    if (evt_type == "LOAD_FILE"):
+        audit_log("downloading file", evt=evt_type)
+        descriptor = "654b9ef2a60a59095511524e"
+
+        config = simple_file.SimpleFileConfiguration()
+        populate_configuration(descriptor, config)
+
+    if (evt_type.startswith("TEST_")):
         audit_log("received a test event", evt=evt_type.removeprefix("TEST_"))
         logger.info("start_crash")
         crash_event_processor(evt)
         logger.info("stop_crash")
 
-        # if(random.choice([True, False])):
-        #     raise ValueError('This randomly failed')
 
 def crash_event_processor(evt: dict):
-   try:
-      logger.info("start")
+    try:
+        logger.info("start")
 
-      path = "/resources/data/configuration.json"
-      # path = "configuration.json"
-      f = open(path)
-      data = json.load(f)
-      f.close()
-      print(data)
-      url = data['connectorKeys']['SHARING_URL']
+        filelocation = "/resources/outputs/output.csv"
 
-      # filelocation="output.csv"
-      filelocation="/resources/outputs/output.csv"
-      
-      logger.info("start")
-      logger.info(url)
+        logger.info("start")
 
-      crash = urllib.request.urlopen(url);
-      content = crash.read().decode('utf-8')
+        crash = urllib.request.urlopen(url)
+        content = crash.read().decode('utf-8')
 
-      with open(filelocation, 'w', newline='') as file:
-         for line in content:
-            file.write(line)
+        with open(filelocation, 'w', newline='') as file:
+            for line in content:
+                file.write(line)
 
-   except Exception as err:
-      logger.error(f"Failed processing event: {err}")
-      logger.error(err)
+    except Exception as err:
+        logger.error(f"Failed processing event: {err}")
+        logger.error(err)
 
 
 if __name__ == "__main__":
-   crash_event_processor({"type": "TEST_CRASH"})
+    crash_event_processor({"type": "TEST_CRASH"})
